f3f3878ad9843410f40af029104bf4dcd6ea9ce7
plugg1N <bbulkaskoricey87@gmail.com>
2024-07-03 21:27:34 +0300

    change parse method

--- downloader.py
+++ downloader.py
@@ -1,11 +1,22 @@
-from pytube import YouTube
-import subprocess
+import yt_dlp as youtube_dl
 import whisper
 import os
 
 # Generate .mp3 file from .mp4 file
-def get_audio(*, filename: str, output_filename: str) -> None:
-    subprocess.run(f'ffmpeg -loglevel error -i "{filename}.mp4" "{output_filename}.mp3"',shell=True)
+def get_audio() -> None:
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    yt_url = "https://www.youtube.com/watch?v=M90JoLZgIlc"
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([yt_url])
 
 
 # Use whisper to translate
@@ -15,27 +26,16 @@ def audio_to_text(*, dest_name: str, model_name: str) -> str:
     return result["text"]
 
 
-# Download video from YouTube 
-def youtube_video_downloader(*, video_url: str, output_filename: str):
-    youtube_video = YouTube(video_url)
-    stream_number = 0
-    streaminfo = youtube_video.streams.filter(file_extension='mp4')
-
-    for s in streaminfo:
-        if 'itag="22"' in str(s):
-            stream_number = 22
-        elif 'itag="18"' in str(s):
-            stream_number = 18
-
-    stream = youtube_video.streams.get_by_itag(stream_number)
-    stream.download(filename=f'{output_filename}.mp4',timeout=12000)
-
-
 
 if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=1aA1WGON49E'
+    url = 'https://www.youtube.com/watch?v=M90JoLZgIlc'
 
-    youtube_video_downloader(video_url=url, output_filename='#1')
-    get_audio(filename='#1', output_filename='#1_audio')
-    os.remove('#1.mp4')
-    print(audio_to_text(model_name='medium', dest_name='#1_audio'))
+
+    # get_audio()
+    # youtube_video_downloader(video_url=url, output_filename='#1')
+    # print('Video loaded')
+    # get_audio(filename='#1', output_filename='#1_audio')
+    audio_to_text(dest_name='hello', model_name='base')
+    # print('Audio received')
+    # os.remove('#1.mp4')
+    # print(audio_to_text(model_name='medium', dest_name='#1_audio'))
