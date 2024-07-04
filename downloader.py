31fec377e712d2ea4dd70e83e0df673944397562
plugg1N <bbulkaskoricey87@gmail.com>
2024-07-04 17:37:09 +0300

    add audio chunking

--- downloader.py
+++ downloader.py
@@ -1,11 +1,36 @@
 import yt_dlp as youtube_dl
-import whisper
+# import whisper
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import librosa
 import os
+
+
+# Split an audio into separate batches
+def split_into_batches(path: str) -> None:
+    audio = AudioSegment.from_file(path)
+
+    chunk_size = int(librosa.get_duration(path=path)) * 100  # in milliseconds
+    overlap = 2000                                           # in milliseconds
+
+    start, end = 0, chunk_size
+
+    num_chunks = (len(audio) - overlap) // (chunk_size - overlap)
+
+    for i in range(num_chunks):
+        start = i * (chunk_size - overlap)
+        end = start + chunk_size
+
+        chunk = audio[start:end]
+        chunk.export(os.path.join('segments', f"chunk_{i}.wav"), format="wav")
+
 
 # Generate .mp3 file from .mp4 file
 def get_audio(link: str) -> None:
     ydl_opts = {
         'format': 'bestaudio/best',
+        'outtmpl': './audio',
+
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -20,11 +45,16 @@ def get_audio(link: str) -> None:
 # Use whisper to translate
 def audio_to_text(*, dest_name: str, model_name: str) -> str:
     model = whisper.load_model(model_name)
-    result = model.transcribe(f'{dest_name}.mp3')
+    result = model.transcribe(f'{dest_name}')
     return result["text"]
+
+
+# Split into batches
 
 
 
 if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=M90JoLZgIlc'
-    
+    url = 'https://www.youtube.com/watch?v=hWTvOa3vifU'
+
+    get_audio(url)
+    split_into_batches('audio.mp3')
