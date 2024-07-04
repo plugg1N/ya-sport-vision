76edd5622c1d1c8df73f99430a29e5d0bd0170dd
plugg1N <bbulkaskoricey87@gmail.com>
2024-07-04 16:49:30 +0300

    minor changes

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import whisper
 import os
 
 # Generate .mp3 file from .mp4 file
-def get_audio() -> None:
+def get_audio(link: str) -> None:
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -13,10 +13,8 @@ def get_audio() -> None:
         }],
     }
 
-    yt_url = "https://www.youtube.com/watch?v=M90JoLZgIlc"
-
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([yt_url])
+        ydl.download([link])
 
 
 # Use whisper to translate
@@ -29,13 +27,4 @@ def audio_to_text(*, dest_name: str, model_name: str) -> str:
 
 if __name__ == '__main__':
     url = 'https://www.youtube.com/watch?v=M90JoLZgIlc'
-
-
-    # get_audio()
-    # youtube_video_downloader(video_url=url, output_filename='#1')
-    # print('Video loaded')
-    # get_audio(filename='#1', output_filename='#1_audio')
-    audio_to_text(dest_name='hello', model_name='base')
-    # print('Audio received')
-    # os.remove('#1.mp4')
-    # print(audio_to_text(model_name='medium', dest_name='#1_audio'))
+    
